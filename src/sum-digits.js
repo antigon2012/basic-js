cc17f023d1d93bf920b53ce34fb5d4618181c3c6
Dzianis Salahub <antigon2012@gmail.com>
2022-04-30 19:14:36 +0300

    feat: done sort by height? sum digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let arr = String(n).split("");
+
+  const sum = arr.reduce((acc, el) => {
+    acc = acc + +el;
+    return acc;
+  }, 0);
+
+  let lengthOfSum = String(sum).length;
+
+  if (lengthOfSum > 1) {
+    return getSumOfDigits(+sum);
+  } else {
+    return +sum;
+  }
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
