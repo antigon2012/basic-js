87c5b653c16874460f774e5cfea8573da27f9a21
Dzianis Salahub <antigon2012@gmail.com>
2022-04-30 23:07:38 +0300

    feat: done common character and delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = String(n).split("");
+  let arrOfNum = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = [...arr];
+    num.splice(i, 1);
+    let x = num.join("");
+    arrOfNum.push(+x);
+  }
+  return arrOfNum.sort((a, b) => a - b).pop();
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
