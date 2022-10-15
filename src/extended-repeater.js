19d4731407067a2c2dfec818629741e24f789c2a
Dzianis Salahub <antigon2012@gmail.com>
2022-10-16 00:14:40 +0300

    fix: fix bug

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,24 +1,24 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater( str, options ) {
-  let elemStr = `${str}${options.additions}${}`
+function repeater(str, options) {
+  // let elemStr = `${str}${options.additions}${}`
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
