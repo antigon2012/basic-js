29ac6796222b26ee43a302cd2f4596a5a35916c2
Dzianis Salahub <antigon2012@gmail.com>
2022-10-17 22:44:54 +0300

    feat: add some selutions of repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,7 +16,47 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function repeater(str, options) {
-  // let elemStr = `${str}${options.additions}${}`
+  let resArr = [];
+  let value = typeof str === "string" ? str : String(str);
+  let repeatTimes = options.repeatTimes,
+    separator = options.separator,
+    addition =
+      typeof options.addition === "string"
+        ? options.addition
+        : String(options.addition),
+    additionRepeatTimes = options.additionRepeatTimes,
+    additionSeparator = options.additionSeparator;
+
+  if (repeatTimes !== 0 || repeatTimes !== undefined) {
+    for (let i = 0; i < repeatTimes; i++) {
+      resArr.push(str);
+      resArr.push(separator ? separator : "+");
+    }
+  }
+  if (addition !== undefined) {
+    if (additionRepeatTimes !== undefined && !isNaN(additionRepeatTimes)) {
+      if (additionRepeatTimes > 0) {
+        let additionRepeat = `${addition}`;
+        for (let j = 0; j < additionRepeatTimes - 1; j++) {
+          let sep = additionSeparator ? additionSeparator : "|";
+          additionRepeat += `${sep}${addition}`;
+        }
+        for (let l = 0; l < resArr.length; l++) {
+          if (resArr[l] === value) {
+            resArr.splice(l + 1, 0, additionRepeat);
+          }
+        }
+      } else {
+        for (let k = 0; k < resArr.length; k++) {
+          if (resArr[k] === value) {
+            resArr.splice(k + 1, 0, addition);
+          }
+        }
+      }
+    }
+  }
+  resArr.splice(resArr.length - 1, 1);
+  return resArr.join("");
 }
 
 module.exports = {
